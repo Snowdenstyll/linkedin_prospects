ec59285e19f5c8989afc1aab72089c41c188f369
Branden Vongphakdy <vbranden_@hotmail.com>
2024-08-16 15:03:47 -0400

    initial commit

---
+++ app/javascript/entrypoints/application.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import mount from "../mount";
+import ReactDOM from "react-dom/client";
+import Product from "../Product";
+
+//const App = () => <h1>Hello from React!</h1>;
+
+const App = () => <h1>Hello from React!</h1>;
+
+mount({
+  App,
+  Product,
+});
